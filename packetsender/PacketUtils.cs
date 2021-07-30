5bef50e3e7d83329ee28c0ed47da5cb9ef058325
Yochran <71285258+Yochran@users.noreply.github.com>
2021-07-30 12:05:40 -0400

    Add files via upload

---
+++ packetsender/PacketUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PacketSender
+{
+    class PacketUtils
+    {
+        public static void sendPackets(String ip, int port, double packets)
+        {
+
+            String allData = "ABCDEFGHIJKLMNOABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+            try
+            {
+                while (true)
+                {
+                    for (int i = 0; i < packets; i++)
+                    {
+                        byte[] packetData = System.Text.ASCIIEncoding.ASCII.GetBytes(allData);
+                        IPEndPoint location = new IPEndPoint(IPAddress.Parse(ip), port);
+                        Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                        client.SendTimeout = 1;
+
+                        client.SendTo(packetData, location);
+                    }
+                }
+            }
+            catch (SystemException)
+            {
+                Console.WriteLine("\n" + "You sent too many packets. (Internet cant handle it.)");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sent {packets} packets to {ip} on {port}.");
+        }
+    }
+}
